8e67ae1f5d73e697bbbd703ed9469ee287d8b405
antoniomalheirs <malheirosann@gmail.com>
2024-10-27 19:27:14 -0300

    Update stuffs

--- customize.sh
+++ customize.sh
@@ -1,4 +1,3 @@
-MODID=template
 AUTOMOUNT=true
 SKIPMOUNT=false
 PROPFILE=false
@@ -15,89 +14,89 @@ REPLACE="
 "
 
 install_files() {
-ui_print " For Android 12/13/14! "
-ui_print " "
-ui_print " Signature verification must be disabled"
-ui_print " mandatory for MIUI 14 users based on"
-ui_print " Android 12/13/14; otherwise, the module will"
-ui_print " not work. "
-ui_print " "
-if [ -n "$KSU" ]; then
-    ui_print "[*] Ambiente: KernelSU"
-    ui_print "[*] Versão Do Modulo para KernelSU: $KSU_VER"
-    ui_print "[*] Versão Do KernelSU: ${KSU_VER_CODE}" 
-else
-    ui_print "[*] Ambiente: Magisk"
-    ui_print "[*] Versão Do Modulo para Magisk: $MAGISK_VER"
-    ui_print "[*] Versão Do Magisk: ${MAGISK_VER_CODE}" 
-fi
-
-Android=`getprop ro.build.version.release`
-
-case "$Android" in
-  12)
-    ui_print "Android 12 detected"
-    cp -rf "$MODPATH/files/plugin/SystemUIPlugin.apk" "$MODPATH/system/app/MiuiSystemUIPlugin"
-    ;;
-  13|14)  # Combines the conditions for versions 13 and 14
-    ui_print "Android 13/14 detected"
-    cp -rf "$MODPATH/files/plugin/SystemUIPlugin.apk" "$MODPATH/system/product/app/MiuiSystemUIPlugin"
-    ;;
-  *)
-    ui_print "Android Version Incompatibility, check on Github for more informations about installation method"
-    ui_print "Exiting the installation to avoid crashing the system"
-    exit 1
-    ;;
-esac
-
-ui_print "- Uninstalling Updates..."
-pm uninstall-system-updates miui.systemui.plugin
-
-TMPAPKDIR=/data/local/tmp
-
-# Copiar e instalar o primeiro APK
-cp -rf "$MODPATH/files/plugin/SystemUIPlugin.apk" "$TMPAPKDIR"
-result1=$(pm install "${TMPAPKDIR}/SystemUIPlugin.apk" 2>&1)
-
-if [ "$result1" != "Success" ]; then
-    ui_print "Error installing SystemUIPlugin.apk: $result1"
-    exit 1
-fi
-
-# Copiar e instalar o segundo APK
-cp -rf "$MODPATH/system/app/PluginExtension/PluginExtension.apk" "$TMPAPKDIR"
-result2=$(pm install "${TMPAPKDIR}/PluginExtension.apk" 2>&1)
-
-if [ "$result2" != "Success" ]; then
-    ui_print "Error installing PluginExtension.apk: $result2"
-    ui_print "You can ignore that ERROR"
-fi
-
-# Copiar e instalar o terceiro APK
-cp -rf "$MODPATH/system/app/Cast/Cast.apk" "$TMPAPKDIR"
-result3=$(pm install "${TMPAPKDIR}/Cast.apk" 2>&1)
-
-if [ "$result3" != "Success" ]; then
-    ui_print "Error installing Cast.apk: $result3"
-    exit 1
-fi
-
-# Verificar se a verificação de assinatura foi desabilitada
-if [[ "$result1" == "Success" &&  "$result3" == "Success" ]]; then
+    ui_print " For Android 12/13/14! "
     ui_print " "
-    ui_print " Desativação da verificação de assinatura detectada"
-    ui_print " Prosseguindo com a instalação como atualização."
-    ui_print " Instalado com sucesso."
-    ui_print " Agora você pode usá-lo sem reiniciar o dispositivo."
+    ui_print " Signature verification must be disabled"
+    ui_print " mandatory for MIUI 14 users based on"
+    ui_print " Android 12/13/14; otherwise, the module will"
+    ui_print " not work. "
     ui_print " "
-    ui_print " "
-else
-    ui_print " "
-    ui_print " Desativação da verificação de assinatura não detectada"
-    ui_print " Prosseguindo com a instalação normal."
-    ui_print " "
-    ui_print " "
-fi
+    if [ -n "$KSU" ]; then
+        ui_print "[*] Ambiente: KernelSU"
+        ui_print "[*] Versão Do Modulo para KernelSU: $KSU_VER"
+        ui_print "[*] Versão Do KernelSU: ${KSU_VER_CODE}" 
+    else
+        ui_print "[*] Ambiente: Magisk"
+        ui_print "[*] Versão Do Modulo para Magisk: $MAGISK_VER"
+        ui_print "[*] Versão Do Magisk: ${MAGISK_VER_CODE}" 
+    fi
+
+    Android=`getprop ro.build.version.release`
+
+    case "$Android" in
+    12)
+        ui_print "Android 12 detected"
+        cp -rf "$MODPATH/files/plugin/SystemUIPlugin.apk" "$MODPATH/system/app/MiuiSystemUIPlugin"
+        ;;
+    13|14) 
+        ui_print "Android 13/14 detected"
+        cp -rf "$MODPATH/files/plugin/SystemUIPlugin.apk" "$MODPATH/system/product/app/MiuiSystemUIPlugin"
+        ;;
+    *)
+        ui_print "Android Version Incompatibility, check on Github for more informations about installation method"
+        ui_print "Exiting the installation to avoid crashing the system"
+        exit 1
+        ;;
+    esac
+
+    ui_print "- Uninstalling Updates..."
+    pm uninstall-system-updates miui.systemui.plugin
+
+    TMPAPKDIR=/data/local/tmp
+
+    # Copiar e instalar o primeiro APK
+    cp -rf "$MODPATH/files/plugin/SystemUIPlugin.apk" "$TMPAPKDIR"
+    result1=$(pm install "${TMPAPKDIR}/SystemUIPlugin.apk" 2>&1)
+
+    if [ "$result1" != "Success" ]; then
+        ui_print "Error installing SystemUIPlugin.apk: $result1"
+        exit 1
+    fi
+
+    # Copiar e instalar o segundo APK
+    cp -rf "$MODPATH/system/app/PluginExtension/PluginExtension.apk" "$TMPAPKDIR"
+    result2=$(pm install "${TMPAPKDIR}/PluginExtension.apk" 2>&1)
+
+    if [ "$result2" != "Success" ]; then
+        ui_print "Error installing PluginExtension.apk: $result2"
+        ui_print "You can ignore that ERROR"
+    fi
+
+    # Copiar e instalar o terceiro APK
+    cp -rf "$MODPATH/system/app/Cast/Cast.apk" "$TMPAPKDIR"
+    result3=$(pm install "${TMPAPKDIR}/Cast.apk" 2>&1)
+
+    if [ "$result3" != "Success" ]; then
+        ui_print "Error installing Cast.apk: $result3"
+        exit 1
+    fi
+
+    # Verificar se a verificação de assinatura foi desabilitada
+    if [[ "$result1" == "Success" &&  "$result3" == "Success" ]]; then
+        ui_print " "
+        ui_print " Desativação da verificação de assinatura detectada"
+        ui_print " Prosseguindo com a instalação como atualização."
+        ui_print " Instalado com sucesso."
+        ui_print " Agora você pode usá-lo sem reiniciar o dispositivo."
+        ui_print " "
+        ui_print " "
+    else
+        ui_print " "
+        ui_print " Desativação da verificação de assinatura não detectada"
+        ui_print " Prosseguindo com a instalação normal."
+        ui_print " "
+        ui_print " "
+    fi
   
 }
 
@@ -113,11 +112,11 @@ cleanup() {
 run_install() {
     unzip -o "$ZIPFILE" -x 'META-INF/*' -d $MODPATH >&2
 	ui_print " "
-	ui_print "- Installing files"
+	ui_print "[*] Installing files"
 	ui_print " "
 	install_files
 	sleep 1
-	ui_print "- Cleaning up"
+	ui_print "[*] Cleaning up"
 	ui_print " "
 	cleanup
 	sleep 1
--- uninstall.sh
+++ uninstall.sh
@@ -3,5 +3,4 @@ rm -rf /data/resource-cache/*
 rm -rf /data/system/package_cache/*
 rm -rf /cache/*
 rm -rf /data/dalvik-cache/*
-pm uninstall com.kashi.pluginExt
 pm uninstall-system-updates miui.systemui.plugin
